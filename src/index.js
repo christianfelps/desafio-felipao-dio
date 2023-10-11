182812b2ae0037f1a0dd4ba4d292636c1035394c
christianfelps <christianfelipialvim@gmail.com>
2023-10-11 06:46:22 -0300

    projeto felipao finalizado

--- src/index.js
+++ src/index.js
@@ -1,37 +1,39 @@
 // Crie uma variável para armazenar o nome e a quantidade de experiência (XP) de um herói, depois utilize uma estrutura de decisão para apresentar alguma das mensagens abaixo:
 
 let nomeDoSeuHeroi = "christinho do poder"
-let expHeroi = 9564
+let expHeroi = 10001
 let rank
 
 if (expHeroi < 0) {
-console.log( "ferro")
+    rank = "ferro"
+    console.log( rank )
 
 
 
 }else if (expHeroi <= 1000){
     rank = "Ferro"
 
-console.log("Ferro") 
+console.log(rank) 
 }else if (expHeroi <=5000 ) {
     rank = "Prata"
 
-console.log("Prata")
+console.log(rank)
 }else if (expHeroi <= 7000 ){
     rank = "Ouro"
-~console.log("Ouro")
+console.log(rank)
 }
 else if (expHeroi <= 8000){
     rank = "Platina"
-    console.log("Platina")
+    console.log(rank)
 }else if(expHeroi <= 9000){
     rank = " Ascendente"
-    console.log("Ascendente")
+    console.log(rank)
 }else if (expHeroi <=10000){
     rank = "Imortal"
-    console.log("Imortal")
+    console.log(rank)
 }else if (expHeroi === 10001){
     rank = "Radiante"
-    console.log("Radiante")
+  
 }
+
 console.log( "O Herói de nome " + nomeDoSeuHeroi + " esta no nivel "+ rank )
